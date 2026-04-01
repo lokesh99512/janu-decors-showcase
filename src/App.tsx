4184e2c58631e3e314e35f4aa9bf1d958bc8c7d3
LAPTOP-CQAVVHJM\Lokesh K <lokeshreddy93817@gmail.com>
2026-04-01 19:38:47 +0530

    changes on vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/janu-decors-showcase/", // ⭐ REQUIRED FOR GH PAGES
+export default defineConfig({
+  base: "/janu-decors-showcase/", // GitHub Pages repo name
 
   server: {
     host: "::",
@@ -16,12 +15,11 @@ export default defineConfig(({ mode }) => ({
 
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  ],
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
